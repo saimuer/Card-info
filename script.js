e874d381ffaf57ae3c3a07169bf3c0eea4513da6
塞缪尔 <122048491+saimuer@users.noreply.github.com>
2024-01-16 19:43:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// .checked = property that determines the checked state of an 
+//            HTML checkbox or radio button element
+
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const payPalBtn = document.getElementById("payPalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function() {
+    if(myCheckBox.checked) {
+        subResult.textContent = `You are subscribed!`;
+    }
+    else {
+        subResult.textContent = `You are not subscribed`;
+    }
+
+    if(visaBtn.checked) {
+        paymentResult.textContent = `Your are paying with Visa`;
+    }
+
+    else if(masterCardBtn.checked) {
+        paymentResult.textContent = `Your are paying with Mastercard`;
+    }
+
+    else if(payPalBtn.checked) {
+        paymentResult.textContent = `Your are paying with PayPal`;
+    }
+
+    else {
+        paymentResult.textContent = `You must select a payment type`;
+    }
+
+}
